d296eeb89a8027ccd298601cee95dfaa2f1c96fa
Becky Zahid <buzahid@microsoft.com>
2024-04-30 10:55:40 -0700

    Added second lab project

---
+++ Allfiles/Labs/02/Starter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel.Plugins.Core;
+#pragma warning disable SKEXP0050 
+#pragma warning disable SKEXP0060
+
+string yourDeploymentName = "";
+string yourEndpoint = "";
+string yourKey = "";
+
+var builder = Kernel.CreateBuilder();
+builder.Services.AddAzureOpenAIChatCompletion(
+    yourDeploymentName,
+    yourEndpoint,
+    yourKey,
+    "gpt-35-turbo-16k");
+var kernel = builder.Build();
+
+StringBuilder chatHistory = new();
+
+kernel.ImportPluginFromType<ConversationSummaryPlugin>();
+var prompts = kernel.ImportPluginFromPromptDirectory("Prompts");
+
